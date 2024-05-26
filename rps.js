ffb3bbd5dec7d603c27d501ca63aca4ea8274058
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-05-26 17:54:22 -0400

    Finalize UI with popup at game end

--- rps.js
+++ rps.js
@@ -1,6 +1,41 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const humanHand = document.querySelector('#human-hand > img');
+const computerHand = document.querySelector('#computer-hand > img');
+
+const humanScoreboard = document.getElementById('human-score');
+const computerScoreboard = document.getElementById('computer-score');
+const roundResultsDisplay = document.getElementById('round-results');
+
+const controlBtns = document.getElementById('rps-controls');
+const btnRock = document.getElementById('rps-ctrl-rock');
+const btnPaper = document.getElementById('rps-ctrl-paper');
+const btnScissors = document.getElementById('rps-ctrl-scissors');
+
+const gameOverPopup = document.getElementById('gameover-popup');
+const btnPlayAgain = document.getElementById('playAgainBtn');
+
+controlBtns.addEventListener("click", function(e){
+    const choice = e.originalTarget.alt;
+    playRound(choice, getComputerChoice());
+});
+
+btnPlayAgain.addEventListener("click", resetGame);
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    gameOverPopup.classList.toggle('active-popup');
+    humanHand.src = `img/rps-placeholder.png`;
+    computerHand.src = `img/rps-placeholder.png`;
+    roundResultsDisplay.textContent = '';
+    roundResultsDisplay.style.display = 'none'
+    humanScoreboard.textContent = '0';
+    computerScoreboard.textContent = '0';
+
+}
+
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     switch (choice) {
@@ -24,6 +59,9 @@ function playRound(humanChoice, computerChoice) {
     const humanSelection = humanChoice.toUpperCase();
     const computerSelection = computerChoice.toUpperCase();
     let roundResult = '';
+
+    humanHand.src = `img/p_${humanChoice.toLowerCase()}.png`;
+    computerHand.src = `img/c_${computerChoice.toLowerCase()}.png`;
 
     // Compare human selection to computer selection and determine winner
     if (humanSelection === 'ROCK') {
@@ -65,27 +103,26 @@ function playRound(humanChoice, computerChoice) {
     }
 
     // Display message with results of the round and update score
+    roundResultsDisplay.style.display = 'inline-block';
     if (roundResult === 'Win') {
-        console.log(`You won! Your ${humanSelection} beat the computer's ${computerSelection}`);
+        roundResultsDisplay.textContent = `Your ${humanSelection} beat the computer's ${computerSelection}`;
         humanScore++;
+        humanScoreboard.textContent = humanScore;
     } else if (roundResult === 'Lose') {
-        console.log(`Sorry, you lost. The computer's ${computerSelection} beat your ${humanSelection}`);
+        roundResultsDisplay.textContent = `The computer's ${computerSelection} beat your ${humanSelection}`;
         computerScore++;
+        computerScoreboard.textContent = computerScore;
     } else if (roundResult === 'Draw') {
-        console.log(`It's a draw! You both chose ${humanSelection}. Let's try that again,`);
-        // redo the current round
-        playRound(getHumanChoice(), getComputerChoice());
+        roundResultsDisplay.textContent = `It's a draw! You both chose ${humanSelection}. Let's try that again,`;
     } else {
-        console.log(`Hmmm... something went wrong. No winner can be declared. Please try again.`);
+        roundResultsDisplay.textContent = `Hmmm... something went wrong. No winner can be declared. Please try again.`;
+    }
+
+    if (humanScore === 5) {
+        gameOverPopup.classList.toggle('active-popup');
+        gameOverPopup.querySelector('.popup-msg').textContent = `Congratulations, you won! You beat the computer with a score of ${humanScore} to ${computerScore}.`;
+    } else if (computerScore === 5) {
+        gameOverPopup.classList.toggle('active-popup');
+        gameOverPopup.querySelector('.popup-msg').textContent = `Sorry, you lost. :( The computer won with a score of ${computerScore} to ${humanScore}.`;
     }
 }
-
-function playGame(){
-    
-    //for (let i=0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    //}
-    console.log(`Your score: ${humanScore} | Computer score: ${computerScore}`);
-}
-
-playGame();
